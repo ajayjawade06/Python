5ab7b742526b3fee2cd127f506e5eac9e727b3cd
ajujawade <ajayjawade06@gmail.com>
2024-02-13 14:03:36 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,53 @@
+s1 = {11,22,33,44,55,66}
+print(s1)
+print(type(s1))
+
+s2 = {1,2,3,4,5,6}
+print(s2)
+print(type(s2))
+
+s3 = {"Geeks" : 1,"For":2,"Geeeks":3}
+print(s3)
+print(type(s3))
+
+s4 = {"Ajay",6,"Jawade",7}
+print(s4)
+print(type(s4))
+
+s5 ={"Ajay",2003,"Jawade",20}
+print(s5)
+
+s6 ={"Ajay",20,"Jawade",2003}
+print(s5)
+
+print(s5==s6)
+
+print(id(s1))
+print(id(s2))
+print(id(s3))
+print(id(s4))
+print(id(s5))
+print(id(s6))
+
+t1 = (11,22,33,44,55,66,11,22,33,44,55)
+print(t1)
+print(type(t1))
+
+t2 = (90)
+print(t2)
+print(type(t2))
+
+t3 = (89,)
+print(t3)
+print(type(t3))
+
+s7={}
+print(s7)
+print(type(s7))
+
+#Subscriptable Means objects which can be accessed using index or key value.
+
+
+
+
+
