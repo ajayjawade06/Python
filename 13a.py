88359534f02951dc8cfa392c03c93ced8053b099
ajujawade <ajayjawade06@gmail.com>
2024-03-06 19:13:51 +0530

    added

--- 13a.py
+++ 13a.py
@@ -42,12 +42,85 @@ a10 = name.title()
 print(a10)
 
 
+full_name = " a"
+
+e3 = full_name.isspace()
+print(e3)
 
 
+name = "ajay"
+e4 = name.capitalize()
+print(e4)
 
 
+e5 = "Iam Learning Js"
+
+print(e5.istitle())
+
+info = ["ajay","jawade","7972235361"]
+e5 = "@".join(info)
+print(e5)
+
+email = "ajayjawade@gmail.com"
+
+e6 = email.split("@")
+print(e6)
 
 
+# Find
 
+email1 = "gmail.com@ajay"
+email2 = "gmail.com@raju"
+email3 = "gmail.com@aditya"
+
+# print(email3.removeprefix("gmail.com@"))
+
+students = [email1,email2,email3]
+
+lista = []
+
+for x in students:
+    q1 = x.removeprefix("gmail.com@")
+    lista.append(q1)
+    
+print(lista)
+
+students = {
+    
+    "1":"ajayadmin",
+    "2":"rajuceo",
+    "3":"kajucustomer",
+    "4":"adityamanager"
+    
+}
+
+roles = ["admin","ceo","customer","manager"]
+names = []
+
+# for name in students.values():
+#     for role in roles:
+#      if role in name:
+#         q2 = name.removesuffix(role)
+#         names.append(q2)
+        
+# print(names)
+
+
+q1 = list(map(lambda x:x.removesuffix("admin"),students.values()))
+print(q1)
+
+
+# swapcase()
+
+a = "hello"
+print(a.swapcase())
+
+# zfill()
+
+name1 = "ajay"
+name2 = "tonyyy"
+
+print(name1.zfill(10))
+print(name2.zfill(10))
 
 
